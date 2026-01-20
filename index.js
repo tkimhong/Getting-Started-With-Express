5b9a684887b5d51aee429ec60aada370d32c6c69
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-01-20 19:36:01 +0700

    Add static middleware

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const app = express();
 
+// TODO: External CSS
+
 // Configure Handlebars as view engine
 app.engine(
   "handlebars",
@@ -11,6 +13,8 @@ app.engine(
 );
 
 app.set("view engine", "handlebars");
+
+app.use(express.static(__dirname + "/public"));
 
 // For organization purposes; it's still called at the end after actual routes
 const errorHandler = (error, request, response, next) => {
