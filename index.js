8ce29b069c0dd4a80bc1fe38bfd06e587d1d53c1
tkimhong <91110603+tkimhong@users.noreply.github.com>
2026-01-20 15:48:52 +0700

    Add routes
    "/", "/about", "/contact"

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (request, response) => {
+  response.type("text/html");
+  response.send("Dididi");
+});
+
+app.get("/about", (request, response) => {
+  response.type("text/html");
+  response.send("About page");
+});
+
+app.get("/contact", (request, response) => {
+  response.type("text/html");
+  response.send("Contact page");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
